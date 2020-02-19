819b620dd433e1feaf119fe6b93c6cd971fa51db
pitikdmitry <pitik.dmitry@mail.ru>
2020-02-19 14:07:17 +0300

    minimum in rotated sorted array wrong answer (with dublicates)

--- binary_search/minimun_in_rotated_sorted_array.py
+++ binary_search/minimun_in_rotated_sorted_array.py
@@ -8,7 +8,7 @@ You may assume no duplicate exists in the array.'''
 from typing import List
 
 
-class Solution:
+class SolutionNoDublicates:
     def findMin(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return -1
@@ -42,6 +42,56 @@ class Solution:
         return -1
 
 
+# s = SolutionNoDublicates()
+# nums = [1, 2]
+# print(s.findMin(nums))
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return -1
+
+        elif len(nums) == 1:
+            return nums[0]
+
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            prev_i = m - 1
+            if prev_i == -1:
+                prev_i = len(nums) - 1
+
+            next_i = m + 1
+            if next_i >= len(nums):
+                next_i = 0
+            a = nums[m]
+            b = nums[l]
+            if nums[m] < nums[prev_i] and nums[m] < nums[next_i]:
+                return nums[m]
+            elif nums[m] == nums[l]:
+                if nums[m] > nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            elif nums[r] < nums[m]:
+                    #   go right
+                l = m + 1
+            else:
+                r = m - 1
+
+        if l == len(nums):
+            return nums[len(nums) - 1]
+        return nums[l]
+
+
 s = Solution()
-nums = [1, 2]
+# nums = [2, 2, 2, 0, 1]
+nums = [3, 5, 1]
+# nums = [1, 3, 5]
 print(s.findMin(nums))
+
+
