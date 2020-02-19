cfbb76f40e398e8e81e5cb01d412cfdfcc53ab1c
pitikdmitry <pitik.dmitry@mail.ru>
2020-02-19 12:22:37 +0300

    solved generate parentheses

--- recursion/generate_parentheses.py
+++ recursion/generate_parentheses.py
@@ -14,43 +14,61 @@ For example, given n = 3, a solution set is:
 from typing import List
 
 
-class Solution:
+class SolutionSlow:
     def check_balance(self, current_str: str) -> bool:
-        stack = []
+        balance = 0
         for c in current_str:
             if c == '(':
-                stack.append(c)
-            elif c == ')':
-                if len(stack) == 0:
-                    return False
+                balance += 1
+            else:
+                balance -= 1
 
-                prev = stack.pop()
-                if prev != '(':
-                    return False
+            if balance < 0:
+                return False
 
-        if len(stack) != 0:
-            return False
-        return True
+        return balance == 0
 
-    def gen_helper(self, n: int, current_str: str, result: set):
+    def gen_helper(self, n: int, start_i: int, current_str: str, result: []) -> None:
         if len(current_str) == 2 * n:
             if self.check_balance(current_str) is True:
-                result.add(current_str)
+                result.append(current_str)
             return
 
-        for i in range(n):
+        for i in range(start_i, 2 * n):
             current_str += '('
-            self.gen_helper(n, current_str, result)
+            self.gen_helper(n, i + 1, current_str, result)
             current_str = current_str[:-1]
 
             current_str += ')'
-            self.gen_helper(n, current_str, result)
+            self.gen_helper(n, i + 1, current_str, result)
             current_str = current_str[:-1]
 
     def generateParenthesis(self, n: int) -> List[str]:
-        result = set()
-        self.gen_helper(n, '', result)
-        return list(result)
+        result = []
+        self.gen_helper(n, 0, '', result)
+        return result
+
+
+s = SolutionSlow()
+print(s.generateParenthesis(3))
+
+
+class Solution:
+    def gen_helper(self, n: int, current_str: str, left: int, right: int, result: List[str]) -> None:
+        if len(current_str) == 2 * n:
+            result.append(current_str)
+            return
+
+        if left < n:
+            self.gen_helper(n, current_str + '(', left + 1, right, result)
+
+        if right < left:
+            self.gen_helper(n, current_str + ')', left, right + 1, result)
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        self.gen_helper(n, '', 0, 0, result)
+        return result
 
 
 s = Solution()
