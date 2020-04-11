ffb08805b57678a40741e1ac38aaef34ffa443a3
pitikdmitry <pitik.dmitry@mail.ru>
2020-04-11 17:10:23 +0300

    solved friend circles

---
+++ MOST_FREQUENT_TASKS/friend_circles.py
@@ -0,0 +1,66 @@
+'''There are N students in a class. Some of them are friends, while some are not.
+Their friendship is transitive in nature. For example, if A is a direct friend of B, and B is a direct friend of C,
+then A is an indirect friend of C. And we defined a friend circle is a group of students who are direct or indirect friends.
+
+Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1,
+then the ith and jth students are direct friends with each other, otherwise not.
+And you have to output the total number of friend circles among all the students.
+
+Example 1:
+Input:
+[[1,1,0],
+ [1,1,0],
+ [0,0,1]]
+Output: 2
+Explanation:The 0th and 1st students are direct friends, so they are in a friend circle.
+The 2nd student himself is in a friend circle. So return 2.
+Example 2:
+Input:
+[[1,1,0],
+ [1,1,1],
+ [0,1,1]]
+Output: 1
+Explanation:The 0th and 1st students are direct friends, the 1st and 2nd students are direct friends,
+so the 0th and 2nd students are indirect friends. All of them are in the same friend circle, so return 1.'''
+from typing import List
+
+
+class Solution:
+    def dfs(self, counter_friends, key, val):
+        q = []
+        q.append(key)
+        while len(q) > 0:
+            key = q.pop(0)
+            arr = counter_friends.get(key)
+
+            if arr is None:
+                continue
+            del counter_friends[key]
+
+            for arr_val in arr:
+                q.append(arr_val)
+
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        counter_friends = {}
+        circles = 0
+
+        for i in range(len(M)):
+            for j in range(0, len(M[i])):
+                if M[i][j] == 1:
+                    if i not in counter_friends:
+                        counter_friends[i] = []
+                    counter_friends[i].append(j)
+
+        while len(counter_friends) > 0:
+            key, val = next(iter(counter_friends.items()))
+            self.dfs(counter_friends, key, val)
+            circles += 1
+        return circles
+
+
+s = Solution()
+M = [[1,0,0,1],
+     [0,1,1,0],
+     [0,1,1,1],
+     [1,0,1,1]]
+print(s.findCircleNum(M))
