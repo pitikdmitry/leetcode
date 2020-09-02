734cf3dffd95db58adbebffa738a51cfd69ab8c8
pitikdmitry <pitik.dmitry@mail.ru>
2020-09-02 18:31:43 +0300

    done alien dictionary second time

--- MOST_FREQUENT_TASKS/alien_dictionary.py
+++ MOST_FREQUENT_TASKS/alien_dictionary.py
@@ -22,6 +22,70 @@ from collections import defaultdict
 from typing import List, Dict
 
 
+#   Solution via topological sort Kahn's algorithm via BFS
+class Solution:
+    def create_graph(self, words: List[str]) -> Dict[str, List]:
+        i, j = 0, 1
+        graph = defaultdict(list)
+        while j < len(words):
+            word_1 = words[i]
+            word_2 = words[j]
+
+            ch_i = 0
+            while ch_i < len(word_1) and ch_i < len(word_2):
+                ch_1 = word_1[ch_i]
+                ch_2 = word_2[ch_i]
+
+                if ch_1 != ch_2:
+                    graph[ch_1].append(ch_2)
+                    if ch_2 not in graph:
+                        graph[ch_2] = []
+                    break
+                ch_i += 1
+
+            i += 1
+            j += 1
+
+        return graph
+
+    def alienOrder(self, words: List[str]) -> str:
+        graph = self.create_graph(words)
+
+        in_degree = defaultdict(int)
+        for key, values in graph.items():
+            if key not in in_degree:
+                in_degree[key] = 0
+
+            for node in values:
+                in_degree[node] += 1
+
+        #   Kahn's algorithm with in_degree array
+        q = []
+        for key, val in in_degree.items():
+            if val == 0:
+                q.append(key)
+
+        result = []
+        while len(q) > 0:
+            node = q.pop(0)
+            result.append(node)
+            for child in graph[node]:
+                in_degree[child] -= 1
+                if in_degree[child] == 0:
+                    q.append(child)
+
+        return ''.join(result)
+
+
+solution = Solution()
+words = ["wrt", "wrf", "er", "ett", "rftt", "te"]
+print(solution.alienOrder(words))
+
+
+from collections import defaultdict
+from typing import List, Dict
+
+
 class Solution:
     #   topological sort via dfs
     def dfs(self, node: str, graph: Dict[str, List[str]], visited: set, call_stack: set, topological_order: List[str]):
@@ -101,69 +165,3 @@ words = ["wrt", "wrf", "er", "ett", "rftt", "te"]
 print(solution.alienOrder(words))
 
 
-from collections import defaultdict
-from typing import List, Dict
-
-
-#   Solution via topological sort Kahn's algorithm via BFS
-class Solution:
-    def add_letters_without_edges(self, word: str, graph: Dict[str, List[str]], in_degree: Dict[str, int]) -> None:
-        for c in word:
-            if c not in graph:
-                graph[c] = []
-            if c not in in_degree:
-                in_degree[c] = 0
-
-    def create_graph(self, words: List[str], graph: Dict[str, List[str]], in_degree: Dict[str, int]) -> None:
-        first_word_idx = 0
-        second_word_idx = 1
-        while second_word_idx < len(words):
-            i = 0
-            first_word, second_word = words[first_word_idx], words[second_word_idx]
-
-            while i < len(first_word) and i < len(second_word):
-
-                if first_word[i] != second_word[i]:
-                    graph[first_word[i]].append(second_word[i])
-
-                    in_degree[second_word[i]] += 1
-                    if first_word[i] not in in_degree:
-                        in_degree[first_word[i]] = 0
-                    break
-                i += 1
-            first_word_idx += 1
-            second_word_idx += 1
-
-        for word in words:
-            self.add_letters_without_edges(word, graph, in_degree)
-
-    def alienOrder(self, words: List[str]) -> str:
-        if words is None or len(words) == 0 or len(words[0]) == 0:
-            return ''
-
-        #   create graph
-        graph, in_degree = defaultdict(list), defaultdict(int)
-        self.create_graph(words, graph, in_degree)
-
-        #   do bfs
-        topological_order = []
-        q = []
-        for node, degree in in_degree.items():
-            if degree == 0:
-                q.append(node)
-
-        while len(q) > 0:
-            node = q.pop(0)
-            topological_order.append(node)
-
-            for child in graph[node]:
-                in_degree[child] -= 1
-                if in_degree[child] == 0:
-                    q.append(child)
-
-        return ''.join(topological_order)
-
-
-solution = Solution()
-words = ["wrt", "wrf", "er", "ett", "rftt", "te"]
-print(solution.alienOrder(words))
