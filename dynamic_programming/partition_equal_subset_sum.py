f1a05de71d97754ca19ce3c72f8f5785d5547150
pitikdmitry <pitik.dmitry@mail.ru>
2020-09-14 22:06:06 +0300

    solved partition equal subset sum top down, bottom up

--- dynamic_programming/partition_equal_subset_sum.py
+++ dynamic_programming/partition_equal_subset_sum.py
@@ -1,11 +1,11 @@
 '''
-Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+Given a non-empty array containing only positive integers,
+find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
 
 Note:
 
 Each of the array element will not exceed 100.
 The array size will not exceed 200.
-
 
 Example 1:
 
@@ -15,11 +15,42 @@ Output: true
 
 Explanation: The array can be partitioned as [1, 5, 5] and [11].
 '''
-from typing import List
+from typing import List, Dict, Tuple
+
+
+#   We go through all values and do two recursive calls either we take it or not
+#   we try to get their sum = sum(nums) // 2
+class Solution:
+    def can_partition_recursive(self, nums: List[int], idx: int, sum: int, memo: Dict[Tuple[int, int], bool]) -> bool:
+        if sum == 0:
+            return True
+        elif idx == len(nums) or sum < 0:
+            return False
+
+        if (idx, sum) in memo:
+            return memo[(idx, sum)]
+
+        #   take
+        c1 = self.can_partition_recursive(nums, idx + 1, sum - nums[idx], memo)
+        #   not take
+        c2 = self.can_partition_recursive(nums, idx + 1, sum, memo)
+        memo[(idx, sum)] = c1 or c2
+        return memo[(idx, sum)]
+
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2 != 0:
+            return False
+        target_sum = sum(nums) // 2
+        return self.can_partition_recursive(nums, 0, target_sum, {})
+
+
+solution = Solution()
+nums = [1, 5, 11, 5]
+print(solution.canPartition(nums))
 
 
 # dp bottom up solution
-class Solution:
+class SolutionBottomUp:
     def canPartition(self, nums: List[int]) -> bool:
         if sum(nums) % 2 != 0:
             return False
@@ -49,6 +80,6 @@ class Solution:
         return dp[len(dp) - 1][len(dp[0]) - 1] != 0
 
 
-solution = Solution()
+solution_bottom_up = SolutionBottomUp()
 nums = [1, 5, 11, 5]
 print(solution.canPartition(nums))
