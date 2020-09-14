943c6921f4b4a013eaa25729e9d4d3df82ea12b7
pitikdmitry <pitik.dmitry@mail.ru>
2020-09-14 21:25:34 +0300

    solved longest common subsequence bottom up

--- dynamic_programming/longest_common_subsequence.py
+++ dynamic_programming/longest_common_subsequence.py
@@ -17,7 +17,10 @@ Explanation: The longest common subsequence is "ace" and its length is 3.
 from typing import Dict, Tuple
 
 
-#   longest common
+#   To find longest common subsequence we start two pointers
+#   First from start of first string, seconds from start of second string
+#   if s1[i1] == s2[i2] we found common character and we increment both pointers
+#   if not we try to find every of them
 class Solution:
     def longest_common_subsequence_recursive(self, text1: str, text2: str, i1: int, i2: int, memo: Dict[Tuple[int, int], int]) -> int:
         if i1 == len(text1) or i2 == len(text2):
@@ -43,3 +46,27 @@ solution = Solution()
 text1 = "abcde"
 text2 = "ace"
 print(solution.longestCommonSubsequence(text1, text2))
+
+
+#   bottom up solution
+class Solution:
+    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+        #   dp table is one element bigger, we use it for not checking border
+        dp = [[0 for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
+
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                #   offset as dp table is one element bigger
+                s1_char = s1[i - 1]
+                s2_char = s2[j - 1]
+                if s1_char == s2_char:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[len(dp) - 1][len(dp[0]) - 1]
+
+
+solution_bottom_up = Solution()
+text1 = "abcde"
+text2 = "ace"
+print(solution_bottom_up.longestCommonSubsequence(text1, text2))
